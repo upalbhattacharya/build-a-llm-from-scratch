18bbe2c6bc9b7e79cdf2b8644177e168ec179037
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-03-04 16:12:36 +0100

    src/build_a_llm_from_scratch/chapter_2_working_with_text_data/simpletokenizerv2.py

--- src/build_a_llm_from_scratch/chapter_2_working_with_text_data/simpletokenizerv2.py
+++ src/build_a_llm_from_scratch/chapter_2_working_with_text_data/simpletokenizerv2.py
@@ -35,6 +35,7 @@ if __name__ == "__main__":
     vocab = tokenizer.tokenizer_regexp.split(raw_text)
     # Create
     vocab = [item.strip() for item in vocab if item.strip()]
+    print(vocab)
     tokenizer.str_to_id = vocab
     test_str = """"It's the last he painted, you know,"
                Mrs. Gisburn said with pardonable pride. Dabadoobie"""
