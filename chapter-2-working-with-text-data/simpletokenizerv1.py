2369577b0345c9c3427ae444e0b219e453e2686e
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-03-04 14:27:33 +0100

    chapter-2-working-with-text-data/simpletokenizerv1.py

--- chapter-2-working-with-text-data/simpletokenizerv1.py
+++ chapter-2-working-with-text-data/simpletokenizerv1.py
@@ -53,9 +53,12 @@ if __name__ == "__main__":
     with open("data/the-verdict.txt", "r", encoding="utf-8") as f:
         raw_text = f.read()
     tokenizer = SimpleTokenizerV1(vocab=None)
-    print(tokenizer.str_to_id)
     vocab = tokenizer.tokenizer_regexp.split(raw_text)
+    # Create
     vocab = [item for item in vocab if item.strip()]
-    print(vocab)
     tokenizer.str_to_id = vocab
-    print(tokenizer.str_to_id)
+    test_str = """It's the last he painted, you know,"
+               Mrs. Gisburn said with pardonable pride."""
+    ids = tokenizer.encode(test_str)
+    print(ids)
+    print(tokenizer.decode(ids))
