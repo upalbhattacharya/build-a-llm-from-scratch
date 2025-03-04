cc22b5bdbb2a9143fc78de02f93c431023907cb7
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-03-04 13:31:01 +0100

    chapter-2-working-with-text-data/tokenizer.py

--- chapter-2-working-with-text-data/tokenizer.py
+++ chapter-2-working-with-text-data/tokenizer.py
@@ -1,1 +1,7 @@
 #!/usr/bin/env python
+
+# Getting text corpus
+
+with open("data/the-verdict.txt", "r", encoding="utf-8") as f:
+    raw_text = f.read()
+print(f"Character Length: {len(raw_text)}")
