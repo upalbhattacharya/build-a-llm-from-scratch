cfc8c3dd40450e75e0accd14683a9f68e8c7e1d3
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-03-04 15:38:49 +0100

    chapter-2-working-with-text-data/simpletokenizerv2.py

--- chapter-2-working-with-text-data/simpletokenizerv2.py
+++ chapter-2-working-with-text-data/simpletokenizerv2.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 
+from typing import Optional
+
 from .simpletokenizerv1 import SimpleTokenizerV1
 
-from typing import Optional
 
 class SimpleTokenizerV2(SimpleTokenizerV1):
     def __init__(self, vocab: Optional[dict] = None):
@@ -10,4 +11,15 @@ class SimpleTokenizerV2(SimpleTokenizerV1):
 
 
 if __name__ == "__main__":
-    
+    with open("data/the-verdict.txt", "r", encoding="utf-8") as f:
+        raw_text = f.read()
+    tokenizer = SimpleTokenizerV2(vocab=None)
+    vocab = tokenizer.tokenizer_regexp.split(raw_text)
+    # Create
+    vocab = [item for item in vocab if item.strip()]
+    tokenizer.str_to_id = vocab
+    test_str = """"It's the last he painted, you know,"
+               Mrs. Gisburn said with pardonable pride."""
+    ids = tokenizer.encode(test_str)
+    print(ids)
+    print(tokenizer.decode(ids))
