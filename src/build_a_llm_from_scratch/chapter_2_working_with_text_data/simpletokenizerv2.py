3292897f29a28c7ebac3627adfa0e13725fe897c
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-03-04 16:04:19 +0100

    src/build_a_llm_from_scratch/chapter_2_working_with_text_data/simpletokenizerv2.py

--- src/build_a_llm_from_scratch/chapter_2_working_with_text_data/simpletokenizerv2.py
+++ src/build_a_llm_from_scratch/chapter_2_working_with_text_data/simpletokenizerv2.py
@@ -10,6 +10,10 @@ from build_a_llm_from_scratch.chapter_2_working_with_text_data.simpletokenizerv1
 class SimpleTokenizerV2(SimpleTokenizerV1):
     def __init__(self, vocab: Optional[dict] = None):
         super().__init__(vocab)
+        if self._str_to_int.get("<|unk|>", None):
+            self._str_to_int.update({"<|unk|>": len(self._str_to_int)
+
+    
 
 
 if __name__ == "__main__":
@@ -18,7 +22,7 @@ if __name__ == "__main__":
     tokenizer = SimpleTokenizerV2(vocab=None)
     vocab = tokenizer.tokenizer_regexp.split(raw_text)
     # Create
-    vocab = [item for item in vocab if item.strip()]
+    vocab = [item.strip() for item in vocab if item.strip()]
     tokenizer.str_to_id = vocab
     test_str = """"It's the last he painted, you know,"
                Mrs. Gisburn said with pardonable pride."""
