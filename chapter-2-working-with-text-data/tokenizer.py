2003011ccf94564a6b5a68b51150e44463541b11
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-03-04 13:34:38 +0100

    chapter-2-working-with-text-data/tokenizer.py

--- chapter-2-working-with-text-data/tokenizer.py
+++ chapter-2-working-with-text-data/tokenizer.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
-# Getting text corpus
+import re
 
+# Loading text corpus
 with open("data/the-verdict.txt", "r", encoding="utf-8") as f:
     raw_text = f.read()
 print(f"Character Length: {len(raw_text)}")
+
+# Basic tokenization
+tokenizer_regexp = re.compile(r"(\s)")
+result = tokenizer_regexp.split(raw_text)
+print(f"Basic Tokenization Length: {len(result)}")
