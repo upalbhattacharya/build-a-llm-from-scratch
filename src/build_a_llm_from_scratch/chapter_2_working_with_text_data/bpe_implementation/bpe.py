c5686281e0722a834c7803543a895331be868872
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-03-07 13:40:47 +0100

    src/build_a_llm_from_scratch/chapter_2_working_with_text_data/bpe_implementation/bpe.py

--- src/build_a_llm_from_scratch/chapter_2_working_with_text_data/bpe_implementation/bpe.py
+++ src/build_a_llm_from_scratch/chapter_2_working_with_text_data/bpe_implementation/bpe.py
@@ -1,3 +1,3 @@
 #!/usr/bin/env python
 
-"""Implementation of BPE encoding following: https://sebastianraschka.com/blog/2025/bpe-from-scratch.html Blah blah"""
+"""Implementation of BPE encoding following: https://sebastianraschka.com/blog/2025/bpe-from-scratch.html"""
