24d52ce4f483815db496f4a2befb15cd49f2733a
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-03-07 14:23:15 +0100

    src/build_a_llm_from_scratch/chapter_2_working_with_text_data/bpe_implementation/bpe.py

--- src/build_a_llm_from_scratch/chapter_2_working_with_text_data/bpe_implementation/bpe.py
+++ src/build_a_llm_from_scratch/chapter_2_working_with_text_data/bpe_implementation/bpe.py
@@ -1,12 +1,7 @@
 #!/usr/bin/env python
 
-"""Implementation of BPE encoding following:
-https://sebastianraschka.com/blog/2025/bpe-from-scratch.html"""
+"""Basic Implemention of BPE algorithm. For learning purposes."""
 
-# 1.1 Bits and bytes
-TEST = "This is some text"
-byte_arr = bytearray(TEST, "utf-8")
-print(byte_arr)
-
-ids = list(byte_arr)
-print(ids)
+import json
+from collections import Counter, deque
+from functools import lru_cache
