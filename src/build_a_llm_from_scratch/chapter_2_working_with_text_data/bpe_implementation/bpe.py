13949b5593b3ee61f44dc22d8ae31a8961ecc4c2
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-03-07 13:43:15 +0100

    src/build_a_llm_from_scratch/chapter_2_working_with_text_data/bpe_implementation/bpe.py

--- src/build_a_llm_from_scratch/chapter_2_working_with_text_data/bpe_implementation/bpe.py
+++ src/build_a_llm_from_scratch/chapter_2_working_with_text_data/bpe_implementation/bpe.py
@@ -3,6 +3,7 @@
 """Implementation of BPE encoding following:
 https://sebastianraschka.com/blog/2025/bpe-from-scratch.html"""
 
+# 1.1 Bits and bytes
 TEST = "This is some text"
 byte_arr = bytearray(TEST, "utf-8")
 print(byte_arr)
