dae2382d82fedc9d380acb0084fdf98c667398fb
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-03-07 13:42:50 +0100

    src/build_a_llm_from_scratch/chapter_2_working_with_text_data/bpe_implementation/bpe.py

--- src/build_a_llm_from_scratch/chapter_2_working_with_text_data/bpe_implementation/bpe.py
+++ src/build_a_llm_from_scratch/chapter_2_working_with_text_data/bpe_implementation/bpe.py
@@ -4,5 +4,5 @@
 https://sebastianraschka.com/blog/2025/bpe-from-scratch.html"""
 
 TEST = "This is some text"
-byte_arr = bytearray(text, "utf-8")
+byte_arr = bytearray(TEST, "utf-8")
 print(byte_arr)
