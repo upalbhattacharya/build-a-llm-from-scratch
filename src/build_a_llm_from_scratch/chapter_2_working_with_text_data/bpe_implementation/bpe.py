aa52c110ce1a50561fbc53d6c5b59a9c0dc09e35
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-03-07 14:33:08 +0100

    src/build_a_llm_from_scratch/chapter_2_working_with_text_data/bpe_implementation/bpe.py

--- src/build_a_llm_from_scratch/chapter_2_working_with_text_data/bpe_implementation/bpe.py
+++ src/build_a_llm_from_scratch/chapter_2_working_with_text_data/bpe_implementation/bpe.py
@@ -5,3 +5,35 @@
 import json
 from collections import Counter, deque
 from functools import lru_cache
+
+
+class BPETokenizerSimple:
+    def __init__(self):
+        pass
+
+    def train(self, *args, **kwargs):
+        pass
+
+    def tokenize(self, token):
+        pass
+
+    def encode(self, text):
+        pass
+
+    def decode(self, token_ids):
+        pass
+
+    def save_vocab_and_merges(self, vocab_path, bpe_merges_path):
+        pass
+
+    def load_vocab_and_merges(self, vocab_path, bpe_merges_path):
+        pass
+
+    def fin_freq_pair(token_ids, mode="most"):
+        pass
+
+    def replace_pair(token_ids, pair_id, new_id):
+        pass
+
+    def get_special_token_id(self, token):
+        pass
