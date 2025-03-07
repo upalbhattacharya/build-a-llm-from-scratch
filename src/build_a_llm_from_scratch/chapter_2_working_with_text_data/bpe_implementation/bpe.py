8a6e42fd118b683417ded793d1dbafe9f88ecf91
Upal Bhattacharya <u.bhattacharya@uu.nl>
2025-03-07 15:00:36 +0100

    src/build_a_llm_from_scratch/chapter_2_working_with_text_data/bpe_implementation/bpe.py

--- src/build_a_llm_from_scratch/chapter_2_working_with_text_data/bpe_implementation/bpe.py
+++ src/build_a_llm_from_scratch/chapter_2_working_with_text_data/bpe_implementation/bpe.py
@@ -21,7 +21,6 @@ class BPETokenizerSimple:
     ):
 
         # Create initial dictionary
-        # First 256 ASCII characters
         unique_chars = [chr(i) for i in range(256)]
         unique_chars.extend(char for char in sorted(set(train_text)) if char not in train_text)
         if " " not in unique_chars:
@@ -39,7 +38,10 @@ class BPETokenizerSimple:
                     self.inverse_vocab[token] = new_id
 
         # Iteratively add pairs
-        token_ids = [self.inverse_vocab[char] for char in train_text
+        token_ids = [self.inverse_vocab[char] for char in train_text]
+
+        for new_id in range(len(self.vocab) vocab_size):
+            pair_id = self.find_freq_pair(token_ids, mode="most")
 
         pass
 
@@ -58,8 +60,16 @@ class BPETokenizerSimple:
     def load_vocab_and_merges(self, vocab_path, bpe_merges_path):
         pass
 
+    @staticmethod
     def find_freq_pair(token_ids, mode="most"):
-        pass
+        pairs = Counter(zip(token_ids, token_ids[1:]))
+
+        if mode == "most":
+            return max(pairs.items(), key=lambda x: x[1])[0]
+        elif mode == "least":
+            return min(pairs.items(), key=lambda x: x[1])[0]
+        else:
+            raise ValueError("Invalid mode. Can only be one of: 'most', 'least'.")
 
     def replace_pair(token_ids, pair_id, new_id):
         pass
